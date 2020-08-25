82361cf7df6f3daef6d68d7fedc5c94b89e08bbe
Mei Hui Goh (NCS) <meihui.goh@ncs.com.sg>
2020-08-25 14:40:46 +0800

    fail the test case-2

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -35,11 +35,6 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
-        assertTrue (stack.isEmpty());
-		stack.push (s1);
-        stack.push (s2);
-		assertFalse (stack.isEmpty());
-		stack.push (s1);
 		assertTrue (stack.isEmpty());
 	}
 
